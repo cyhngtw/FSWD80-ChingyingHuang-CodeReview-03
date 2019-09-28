26b76632513f019f776672a22698771e38ec7410
Ching ying Huang <cyhngtw@gmail.com>
2019-09-28 15:40:00 +0200

    review3

---
+++ review3.js
@@ -0,0 +1,28 @@
+
+
+
+function calculateInsurance() {
+
+	var name = document.getElementById('nome').value;
+
+	var selection = document.getElementById('paese')
+
+	var horsepower = parseInt(document.getElementById('calvalo').value);
+
+	var age = parseInt(document.getElementById('eta').value);
+
+	if (selection = 'Austria') {
+		var Cal="hi "+name + ",   your insurance cost is  "+Math.floor((horsepower * 100)/(age)+50) + "€";
+	}
+
+	else if (selection ='Hungary') {
+		var Cal="hi "+name+",  your insururance cost is  "+Math.floor((horsepower *120)/(age)+100)+" €";
+	}
+
+	else if (selection = 'Greece') {
+		var Cal="hi "+name+",  your insururance cost is "+Math.floor((horsepower *130)/(age+3)+50)+" €";
+	}
+
+	
+document.getElementById('result').innerHTML=Cal;
+}
